07c2157e19abfc489e9493f61cf60f9d4e21d6f5
Jonas Karlman <jonas@kwiboo.se>
2015-03-15 22:01:12 +0100

    Smoother background transition when changing section

--- xbmc/guilib/GUIMultiImage.cpp
+++ xbmc/guilib/GUIMultiImage.cpp
@@ -191,8 +191,7 @@ bool CGUIMultiImage::OnMessage(CGUIMessage &message)
       m_plexLabels[list->Get(i)->GetPath()] = list->Get(i)->GetLabel();
     }
     
-    m_directoryStatus = UNLOADED;
-    AllocResources();
+    LoadDirectory();
     
     return true;
   }
