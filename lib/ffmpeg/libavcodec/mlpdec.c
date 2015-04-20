fa6a301f620bf2764f628e3d2bec277b9e41dc45
Tobias Hieta <tobias@plexapp.com>
2015-04-20 10:16:54 +0200

    Revert "Hopefully apply patch that fixes Dolby Atmos streams"
    This reverts commit 2341a88e19f9d947e8c87b4b2d61a6f3a6ae9e34.

--- lib/ffmpeg/libavcodec/mlpdec.c
+++ lib/ffmpeg/libavcodec/mlpdec.c
@@ -318,9 +318,7 @@ static int read_major_sync(MLPDecodeContext *m, GetBitContext *gb)
     m->access_unit_size_pow2 = mh.access_unit_size_pow2;
 
     m->num_substreams        = mh.num_substreams;
-
-    /* limit to decoding 3 substreams, as the 4th is used by Dolby Atmos for non-audio data */
-    m->max_decoded_substream = FFMIN(m->num_substreams - 1, 2);
+    m->max_decoded_substream = m->num_substreams - 1;
 
     m->avctx->sample_rate    = mh.group1_samplerate;
     m->avctx->frame_size     = mh.access_unit_size;
