36012f317de2e5a52386d2f92088536ac17b4471
Lionel CHAZALLON <LongChair@hotmail.com>
2015-05-06 08:13:58 +0200

    Remove Vevo Overlay on Playback Window startup as well, should fix 1557

--- xbmc/video/windows/GUIWindowFullScreen.cpp
+++ xbmc/video/windows/GUIWindowFullScreen.cpp
@@ -772,6 +772,10 @@ bool CGUIWindowFullScreen::OnMessage(CGUIMessage& message)
   {
   case GUI_MSG_WINDOW_INIT:
     {
+      /* PLEX */
+      deleteOverlays();
+      /* END PLEX */
+
       // check whether we've come back here from a window during which time we've actually
       // stopped playing videos
       if (message.GetParam1() == WINDOW_INVALID && !g_application.IsPlayingVideo())
