8f9203e7e8f7aae9a0aacb6bc6107a09871f1394
Jonas Karlman <jonas@kwiboo.se>
2016-01-30 11:02:15 +0100

    Backport guilib fixes

--- xbmc/guilib/GUIWindowManager.h
+++ xbmc/guilib/GUIWindowManager.h
@@ -33,6 +33,7 @@
 #include "IMsgTargetCallback.h"
 #include "DirtyRegionTracker.h"
 #include "utils/GlobalsHandling.h"
+#include <list>
 
 class CGUIDialog;
 
@@ -51,7 +52,6 @@ public:
   bool SendMessage(int message, int senderID, int destID, int param1 = 0, int param2 = 0);
   bool SendMessage(CGUIMessage& message, int window);
  /* PLEX */
-  int  RemoveThreadMessageByMessageIds(int *pMessageIDList);
   void setRetrictedAccess(bool restricted) { m_restrictedAccessMode = restricted; }
   bool isAccessRestricted() { return m_restrictedAccessMode; }
   /* END PLEX */
@@ -123,9 +123,11 @@ public:
   void RemoveDialog(int id);
   int GetTopMostModalDialogID(bool ignoreClosing = false) const;
 
-  void SendThreadMessage(CGUIMessage& message);
-  void SendThreadMessage(CGUIMessage& message, int window);
+  void SendThreadMessage(CGUIMessage& message, int window = 0);
   void DispatchThreadMessages();
+  // method to removed queued messages with message id in the requested message id list.
+  // pMessageIDList: point to first integer of a 0 ends integer array.
+  int RemoveThreadMessageByMessageIds(int *pMessageIDList);
   void AddMsgTarget( IMsgTargetCallback* pMsgTarget );
   int GetActiveWindow() const;
   int GetFocusedWindow() const;
@@ -170,7 +172,7 @@ private:
   std::stack<int> m_windowHistory;
 
   IWindowManagerCallback* m_pCallback;
-  std::vector < std::pair<CGUIMessage*,int> > m_vecThreadMessages;
+  std::list < std::pair<CGUIMessage*,int> > m_vecThreadMessages;
   CCriticalSection m_critSection;
   std::vector <IMsgTargetCallback*> m_vecMsgTargets;
 
