0b63f87e2c2090a26782fc6fe6cfa8382a1e1b4c
Jonas Karlman <jonas@kwiboo.se>
2016-02-16 23:42:31 +0100

    Dual sign binaries on windows using post build event

--- plex/scripts/WindowsSign.cmd
+++ plex/scripts/WindowsSign.cmd
@@ -8,16 +8,24 @@ setlocal
 
 set TargetFile=%~1
 
-signtool.exe sign /a /sm /t http://timestamp.verisign.com/scripts/timstamp.dll "%TargetFile%"
+signtool.exe sign /a /sm /fd sha1 /t http://timestamp.comodoca.com "%TargetFile%"
 if "%errorlevel%" == 1 (
-  echo FATAL ERROR - could sign %TargetFile%
+  echo FATAL ERROR - could not sign %TargetFile%
   exit /b 1
 )
 
-signtool.exe verify /pa "%TargetFile%"
+signtool.exe sign /a /sm /as /fd sha256 /tr http://timestamp.comodoca.com /td sha256 "%TargetFile%"
+if "%errorlevel%" == 1 (
+  echo FATAL ERROR - could not sign %TargetFile%
+  exit /b 1
+)
+
+signtool.exe verify /all /pa "%TargetFile%"
 if "%errorlevel%" == 1 (
   echo FATAL ERROR - could not verify signature for %TargetFile%
   exit /b 1
 )
 
 endlocal
+
+exit /b 0
