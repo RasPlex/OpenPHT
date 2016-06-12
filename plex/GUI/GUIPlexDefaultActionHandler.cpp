bde8df22773492f6e2efbef2cefa6ca4623be783
Jonas Karlman <jonas@kwiboo.se>
2016-06-12 21:47:49 +0200

    Use overlay image to determine watched / unwatched state
    See RasPlex/OpenPHT#77

--- plex/GUI/GUIPlexDefaultActionHandler.cpp
+++ plex/GUI/GUIPlexDefaultActionHandler.cpp
@@ -520,9 +520,7 @@ void CGUIPlexDefaultActionHandler::GetContextButtonsForAction(int actionID, CFil
     {
       if (item->IsVideo() && item->IsPlexMediaServer())
       {
-        CStdString viewOffset = item->GetProperty("viewOffset").asString();
-        
-        if (item->GetVideoInfoTag()->m_playCount == 0)
+        if (item->GetOverlayImageID() != CGUIListItem::ICON_OVERLAY_WATCHED)
           buttons.Add(actionID, 16103);
       }
       break;
@@ -532,9 +530,7 @@ void CGUIPlexDefaultActionHandler::GetContextButtonsForAction(int actionID, CFil
     {
       if (item->IsVideo() && item->IsPlexMediaServer())
       {
-        CStdString viewOffset = item->GetProperty("viewOffset").asString();
-        
-        if (item->GetVideoInfoTag()->m_playCount > 0 || viewOffset.size() != 0)
+        if (item->GetOverlayImageID() != CGUIListItem::ICON_OVERLAY_UNWATCHED)
           buttons.Add(actionID, 16104);
       }
       break;
