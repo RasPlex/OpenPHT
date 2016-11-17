lkosmid/xbmc

Fixed compilation of refactored EGL on X11

xbmc/windowing/WindowingFactory.h
@@ -29,7 +29,10 @@
 #elif defined(TARGET_WINDOWS) && defined(HAS_DX)
 #include "windows/WinSystemWin32DX.h"
 
-#elif defined(TARGET_LINUX)   && defined(HAVE_X11)
+#elif defined(TARGET_LINUX)   && defined(HAVE_X11)   && defined(HAS_GLES)
+#include "X11/WinSystemX11GLContextEGL.h"
+
+#elif defined(TARGET_LINUX)   && defined(HAVE_X11)   && defined(HAS_GL)
 #include "X11/WinSystemX11GLContext.h"
 
 #elif defined(TARGET_LINUX)   && defined(HAS_GLES) && defined(HAS_EGL) && !defined(HAVE_X11)

RasPlex/OpenPHT

xbmc/windowing/WindowingFactory.h
@@ -29,7 +29,10 @@
 #elif defined(TARGET_WINDOWS) && defined(HAS_DX)
 #include "windows/WinSystemWin32DX.h"
 
-#elif defined(TARGET_LINUX)   && defined(HAVE_X11)
+#elif defined(TARGET_LINUX)   && defined(HAVE_X11)   && defined(HAS_GLES)
+#include "X11/WinSystemX11GLESContext.h"
+
+#elif defined(TARGET_LINUX)   && defined(HAVE_X11)   && defined(HAS_GL)
 #include "X11/WinSystemX11GLContext.h"
 
 #elif defined(TARGET_LINUX)   && defined(HAS_GLES) && defined(HAS_EGL) && !defined(HAVE_X11)